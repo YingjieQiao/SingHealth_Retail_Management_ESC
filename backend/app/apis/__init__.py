a95e5278f3c8c0603e8d0d48ce58d52728c2afc1
yingjie <yingjie_qiao@outlook.com>
2021-02-15 11:18:00 +0800

    feat: upload and download with s3

--- backend/app/apis/__init__.py
+++ backend/app/apis/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, current_app, jsonify, request
 import time
 import boto3
+from botocore.exceptions import ClientError
+import logging
 
 
 apis = Blueprint('apis', __name__)
@@ -16,10 +18,47 @@ def get_current_time():
     return {'time': time.time()}
 
 
-@apis.route('/upload_pic')
-def upload_pic(path):
-    s3 = boto3.resource('s3')   
-    data = open(path, 'rb')
-    s3.Bucket('escapp-bucket').put_object(Key='images/testpic.png', Body=data)
+@apis.route('/upload_file')
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
 
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    if object_name is None:
+        object_name = file_name
+
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+        #TODO log the response in the logger
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+@apis.route('/upload_file')
+def download_file(file_name, bucket, object_name):
+    """Download a file from an S3 bucket
+
+    :param file_name: File to download
+    :param bucket: Bucket to download from
+    :param object_name: S3 object name.
+    :return: True if file was downloaded, else False
+    """
+
+    if object_name is None:
+        return False
+
+    s3 = boto3.client('s3')
+    try:
+        response = s3.download_file(bucket, object_name, file_name)
+        #TODO log the response in the logger
+    except ClientError as e:
+        logging.error(e)
+        return False
     return True
--- backend/tests/test_s3.py
+++ backend/tests/test_s3.py
@@ -1,14 +1,50 @@
 import boto3
-
-def main():
-    # Let's use Amazon S3
-    s3 = boto3.resource('s3')   
+from botocore.exceptions import ClientError
+import logging
 
 
-    # Upload a new file
-    data = open('testpic.png', 'rb')
-    s3.Bucket('escapp-bucket').put_object(Key='images/testpic.png', Body=data)
+def upload(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    if object_name is None:
+        object_name = file_name
+
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+def download(file_name, bucket, object_name):
+    """Download a file from an S3 bucket
+
+    :param file_name: File to download
+    :param bucket: Bucket to download from
+    :param object_name: S3 object name.
+    :return: True if file was downloaded, else False
+    """
+
+    if object_name is None:
+        return False
+
+    s3 = boto3.client('s3')
+    try:
+        response = s3.download_file(bucket, object_name, file_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 
 if __name__ == "__main__":
-    main()
+    # upload('testpic.png', 'escapp-bucket', 'images/test2.png')
+    download('downloaded_pic2.png', 'escapp-bucket', 'images/testpic.png')
