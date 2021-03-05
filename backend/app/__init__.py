efb9dac0c3a3c81eac17bb7126400a9151e7113b
Mihir Chhiber <mihir_chhiber@mymail.sutd.edu.sg>
2021-03-05 10:30:34 +0800

    email with working attachments

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,12 +3,7 @@ from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from app.models import db
 from app.config import Config
-from itsdangerous import URLSafeTimedSerializer
-from flask_mail import Mail
 
-# mail = Mail()
-
-mail = Mail()
 def create_app(config_class=Config):
     app = Flask(__name__)
     
@@ -21,8 +16,6 @@ def create_app(config_class=Config):
     
     from app.apis import apis
     app.register_blueprint(apis)
-
-    mail.init_app(app)
 
     return app
 
--- backend/app/config.py
+++ backend/app/config.py
@@ -3,11 +3,4 @@ class Config():
     MONGODB_SETTINGS = {
         'host': 'mongodb://localhost/escapp'
     }
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USERNAME = 'starboypp69@gmail.com'
-    MAIL_PASSWORD = 'MDR-XB450AP'
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_ASCII_ATTACHMENTS = True
     
