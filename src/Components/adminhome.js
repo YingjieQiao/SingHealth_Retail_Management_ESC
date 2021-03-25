3f4ad900ffb59f3d670aa016dc3cc9b5dec77b95
ishaannair <73781379+ishaannair@users.noreply.github.com>
2021-03-25 22:57:21 +0800

    Update adminhome.js

--- src/Components/adminhome.js
+++ src/Components/adminhome.js
@@ -165,7 +165,7 @@ class Adminhome extends Component {
            <td>{notes}</td>
            <td>{staffName}</td>
            <td>{tenantName}</td>
-           <td>{rectified}</td>
+           <td>{rectified?"True":"False"}</td>
         </tr>
      )
   })
