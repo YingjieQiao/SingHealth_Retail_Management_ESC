1715036fb90ab1f0c7e58daa0bd0b7f106b8b25f
ishaannair <73781379+ishaannair@users.noreply.github.com>
2021-03-26 04:42:14 +0800

    test 1

--- src/tests/testing.py
+++ src/tests/testing.py
@@ -2,12 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import re
 import time
+from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome("\webdriver\\chromedriver") # Get local session of firefox
 browser.get("http://localhost:3000/") # Load App page
+time.sleep(3)
 browser.find_element(By.LINK_TEXT,"Register").click()
 time.sleep(3)
-browser.quit()
+browser.find_element(By.LINK_TEXT,"Login").click()
+time.sleep(3)
+#browser.find_element(By.LINK_TEXT,"admin").click()
+time.sleep(3)
+print("yolo")
+links = browser.find_elements_by_tag_name('a')
+print("yolo1w2212")
+print(links)
+time.sleep(3)				
+print("***Printing all link names***")
+# print all the links
+for elem in links:
+    href = elem.get_attribute('href')
+    if href is not None:
+        print(href)
+time.sleep(3)	
+print("***Printing all link addresses***")
+# print all the hyper links
+# for  i in range(links.size()) :
+# 	print(i + " " + links.get(i).getAttribute("href"))
+# time.sleep(3)	
+# browser.quit()
 # elem = browser.find_element_by_name("LoginID") # Find the Login box
 # elem.send_keys("Administrator")
 # elem = browser.find_element_by_name("Password") # Find the Password box
