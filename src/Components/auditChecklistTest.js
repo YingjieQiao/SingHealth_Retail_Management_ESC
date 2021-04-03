ac9075f6eef6606eab244731ea9aa0c255751692
24kmystique <56757916+24kmystique@users.noreply.github.com>
2021-04-03 22:28:32 +0800

    Update auditChecklistTest.js

--- src/Components/auditChecklistTest.js
+++ src/Components/auditChecklistTest.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import styles from "./CSS/auditForm.module.css";
 
 
 class AuditChecklistTest extends Component {
@@ -19,7 +20,8 @@ class AuditChecklistTest extends Component {
         },
         comment: "",
         auditeeArray: [],
-        numOfAuditee: []
+        numOfAuditee: [],
+        options: [0,1,2,3,4,5,6,7,8,9,10]
     }
 
     componentDidMount() {
@@ -44,18 +46,23 @@ class AuditChecklistTest extends Component {
 
         return (
             <div>
-                <h2>New Audit</h2>
-                <h2>Audit Checklist (Non-F&#38;B)</h2>
-                <form>
-                    <div>
-                        <label>Auditee:</label>
-                        <select class="custom-select my-1 mr-sm-2" onChange={this.saveAuditee}>
+                <form className={styles.form}>
+                    <div className={styles.qn_body}>
+                        <label className={styles.title}>New Audit</label>
+                        <label className={styles.form_qn}>Audit Checklist (Non-F&#38;B)</label>
+                    </div>
+
+                    <div className={styles.qn_body} >
+                        <label className={styles.form_qn}>Auditee:</label>
+                        <select className={styles.form_qn} class="custom-select my-1 mr-sm-2" onChange={this.saveAuditee}>
                             <option selected>Choose...</option>
                             { this.state.numOfAuditee.map(index => <option value={index.toString()}>{this.handleAuditee(index)}</option> ) }
                         </select>
                     </div>
-                    <div>
-                        <label>Auditor:</label>
+
+                    
+                    <div className={styles.qn_body} >
+                        <label className={styles.form_qn}>Auditor:</label>
                         <select class="custom-select my-1 mr-sm-2" id="auditorName" onChange={this.handleAuditor}>
                             <option selected value="-1">Choose...</option>
                             <option value="Tom">Tom</option>
@@ -63,8 +70,8 @@ class AuditChecklistTest extends Component {
                             <option value="Charlie">Charlie</option>
                         </select>
                     </div>
-                    <div>
-                        <label>Auditor's Department:</label>
+                    <div className={styles.qn_body}>
+                        <label className={styles.form_qn}>Auditor's Department:</label>
                         <select class="custom-select my-1 mr-sm-2" id="auditorDepartment" onChange={this.handleDepartment}>
                             <option selected value="-1">Choose...</option>
                             <option value="CSR">CSR</option>
@@ -72,39 +79,63 @@ class AuditChecklistTest extends Component {
                             <option value="Risk">Risk</option>
                         </select>
                     </div>
-
-                    <h3>1. Professionalism &#38; Staff Hygiene (10%)</h3>
-                    <h4>Professionalism</h4>
-                    <div class="form-group">
-                        <label>Shop is open and ready to service patients/visitors according to operating hours.</label>
-                        <input type="number" min="0" max="10" pattern='^([0-9]|([1-9][0-9])|10)$' id="001" onInput={this.saveScore}/>
+                    
+                    <div className={styles.qn_body}>
+                        <label className={styles.heading}>Part 1: Professionalism &#38; Staff Hygiene (10%)</label>
+                        <label className={styles.form_qn}>Professionalism</label>
+                    </div>
+                    <div class="form-group" className={styles.qn_body}>
+                        <label className={styles.form_qn}>Shop is open and ready to service patients/visitors according to operating hours.</label>
+                        <div><label>Lowest score</label>
+                        {this.state.options.map(index => {return (
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="001" id="001" onInput={this.saveScore} value={index} />
+                            <label class="form-check-label" >{index}</label>
+                        </div>)})}
+                        <label>Highest score</label></div>
                     </div>
 
-                    <h3>2. Housekeeping &#38; General Cleanliness (40%)</h3>
-                    <h4>General Environment Cleanliness</h4>
-                    <div class="form-group">
-                        <label>Adequate and regular pest control. Pest control record.</label>
-                        <input type="number" min="0" max="10" pattern='^([0-9]|([1-9][0-9])|10)$' id="007" onInput={this.saveScore}/>
+                    <div className={styles.qn_body}>
+                        <label className={styles.heading}>Part 2: Housekeeping &#38; General Cleanliness (40%)</label>
+                        <label className={styles.form_qn}>General Environment Cleanliness</label>
+                    </div>
+                    <div class="form-group" className={styles.qn_body}>
+                        <label className={styles.form_qn}>Adequate and regular pest control. Pest control record.</label>
+                        <div><label>Lowest score</label>
+                        {this.state.options.map(index => {return (
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="007" id="007" onInput={this.saveScore} value={index} />
+                            <label class="form-check-label" >{index}</label>
+                        </div>
+                    )})}
+                    <label>Highest score</label></div>            
                     </div>
 
-                    <h3>Workplace Safety &#38; Health (40%)</h3>
-                    <h4>General Safety</h4>
-                    <div class="form-group">
-                        <label>MSDS for all industrial chemicals are available and up to date.</label>
-                        <input type="number" min="0" max="10" pattern='^([0-9]|([1-9][0-9])|10)$' id="019" onInput={this.saveScore}/>
+                    <div className={styles.qn_body}>
+                        <label className={styles.heading}>Part 3: Workplace Safety &#38; Health (40%)</label>
+                        <label className={styles.form_qn}>General Safety</label>
+                    </div>
+                    <div class="form-group" className={styles.qn_body}>
+                        <label className={styles.form_qn}>MSDS for all industrial chemicals are available and up to date.</label>
+                        <div>
+                        <label>Lowest score</label>
+                        {this.state.options.map(index => {return (
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="019" id="019" onInput={this.saveScore} value={index} />
+                            <label class="form-check-label" >{index}</label>
+                        </div>
+                    )})}
+                        <label>Highest score</label>
+                    </div>                    
                     </div>
 
-                    <button type="button" class={this.getButtonClasses()} onClick={this.tabulateScore}>Tabulate scores</button>
+                    <div className={styles.qn_body}>
+                        <label className={styles.heading}>Comments:</label>
+                        <input className={styles.commentInput} onInput={this.saveComment} type="text" />
+                    </div>
+                    <button type="submit" class={this.getButtonClasses()} onClick={this.handleSubmit}>Submit</button>
 
-                    <h4>Scoring</h4>
-                    <p>Professionalism &#38; Staff Hygiene: {this.state.profStaffHydScore} /20%</p>
-                    <p>Housekeeping &#38; General Cleanliness: {this.state.houseGeneralScore} /40%</p>
-                    <p>Workplace Safety &#38; Health: {this.state.workSafetyHealthScore} /40%</p>
-                    <p>Total Score: {this.state.totoalScore} /100%</p>
-                    <label>Comments:</label>
-                    <input onInput={this.saveComment} type="text" />
                 </form>
-                <button type="submit" class={this.getButtonClasses()} onClick={this.handleSubmit}>Submit</button>
 
             </div>
         )
@@ -164,48 +195,13 @@ class AuditChecklistTest extends Component {
 
     saveScore = event => {
         var newScoreDict = this.state.scoreDict;
-        if (event.target.value >= 0) {
-            newScoreDict[event.target.id] = event.target.value;
+        const val = parseInt(event.target.value);
+        if (val >= 0) {
+            newScoreDict[event.target.id] = val;
         } else {
             newScoreDict[event.target.id] = 0;
         }
         this.setState({scoreDict: newScoreDict});
-        this.updateSectionScore(event.target.id, event.target.value);
-    }
-
-    updateSectionScore = (qnId, qnValue) => {
-        let newProfStaffHydScore = this.state.profStaffHydScore;
-        let newHouseGeneralScore = this.state.houseGeneralScore;
-        let newWorkSafetyHealthScore = this.state.workSafetyHealthScore;
-        let newFinalDict = this.state.finalDict;
-
-        // formula: score = (currentScore  / (numQ * maxScoreForOneQ) ) * weightageForTheSection
-        if (qnId <= 6) {
-            newProfStaffHydScore += parseInt(qnValue);
-            newProfStaffHydScore = (newProfStaffHydScore / 10) * 20;
-            newFinalDict["profStaffHydScore"] = newProfStaffHydScore;
-
-        }
-        else if (qnId >= 7 &&  qnId <= 18) {
-            newHouseGeneralScore += parseInt(qnValue);
-            newHouseGeneralScore = (newHouseGeneralScore / 10) * 40;
-            newFinalDict["houseGeneralScore"] = newHouseGeneralScore;
-        }
-        else if (qnId >= 19) {
-            newWorkSafetyHealthScore += parseInt(qnValue);
-            newWorkSafetyHealthScore = (newWorkSafetyHealthScore / 10) * 40;
-            newFinalDict["workSafetyHealthScore"] = newWorkSafetyHealthScore;
-        }
-
-        let total = newProfStaffHydScore + newHouseGeneralScore + newHouseGeneralScore;
-
-        this.setState({
-            profStaffHydScore: newProfStaffHydScore,
-            houseGeneralScore: newHouseGeneralScore,
-            workSafetyHealthScore: newHouseGeneralScore,
-            totoalScore: total,
-            finalDict: newFinalDict
-        });
     }
 
     saveComment = event => {
@@ -216,7 +212,6 @@ class AuditChecklistTest extends Component {
 
     handleSubmit = event  => {
         event.preventDefault();
-        console.log("final: ", this.state.finalDict);
 
         if (Object.keys(this.state.scoreDict).length < (this.state.dataLength - 1)) {
             console.log("empty field");
@@ -224,7 +219,7 @@ class AuditChecklistTest extends Component {
         } else { 
             // all data has been filled
             // proceeds to send data to backend
-            
+            this.tabulateScore();
             const headers = {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Access-Control-Allow-Origin': '*'
@@ -238,7 +233,7 @@ class AuditChecklistTest extends Component {
         }
     }
 
-    tabulateScore = event => {
+    tabulateScore = () => {
         if (Object.keys(this.state.scoreDict).length < (this.state.dataLength - 1)) {
             console.log("empty field");
         } else {
