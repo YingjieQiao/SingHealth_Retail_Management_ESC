82553944d77647989d8f0f142e5b44aca38bc3e4
24kmystique <56757916+24kmystique@users.noreply.github.com>
2021-04-04 15:17:34 +0800

    Update tenantHome.js

--- src/Components/tenantHome.js
+++ src/Components/tenantHome.js
@@ -15,12 +15,17 @@ import axios from 'axios';
 
 class tenantHome extends Component { 
 
+  state = {
+    data: null,
+    readStatus: "Unread"
+  }
+
   componentDidMount() {
     axios.get("http://localhost:5000/tenant_get_photo_notification")
     .then(
         res => {
             console.log(res);
-
+            this.setState({data: res.data});
         }
     )
   }
@@ -30,9 +35,71 @@ class tenantHome extends Component {
       <div className='home'>
         <TenantNavbar/>
         <h1>Tenant User Homepage</h1>
+        <div>
+          <button type="button" class={this.getButtonClasses()} onClick={this.handleRead}>{this.state.readStatus}</button>
+        </div>
       </div>
     )
   }
+
+  displayInfo() {
+    if (true) {
+      //display
+    }
+  }
+
+  handleRead = event => {
+    this.setState({readStatus: "Read"});
+
+    try {
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin': '*'
+      };
+  
+      axios.post("http://localhost:5000/tenant_read_photo_notification", this.state.data, headers
+      ).then(res => {
+  
+       }); 
+    } catch (e) {
+
+    }
+
+  }
+
+  handleDelete = event => {
+    // false to true
+
+    try {
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin': '*'
+      };
+  
+      axios.post("http://localhost:5000/tenant_delete_photo_notification", this.state.data, headers
+      ).then(res => {
+  
+       }); 
+    } catch (e) {
+
+    }
+  }
+
+  validateReadStatus = () => {
+    const status = this.state.readStatus;
+    if (status === "Read") {
+      return true;
+    } else  {
+      return false;
+    }
+  }
+
+  getButtonClasses() {
+    let classes = 'btn btn-';
+    classes += this.validateReadStatus() === false ? 'light' : 'primary';
+    return classes;
+  }
+
 }
 
 export default tenantHome;
