5b31dffd9818463c6dec5117abbb8c9d647274a2
24kmystique <56757916+24kmystique@users.noreply.github.com>
2021-04-04 14:52:03 +0800

    Update tenantHome.js

--- src/Components/tenantHome.js
+++ src/Components/tenantHome.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TenantNavbar from './Tenant_Navbar';
+import axios from 'axios';
 
-function tenantHome() {
-  localStorage.setItem("usertype","tenant")  ;
-  return (
-    <div className='home'>
-            <TenantNavbar/>
+
+// function tenantHome() {
+//   localStorage.setItem("usertype","tenant")  ;
+//   return (
+//     <div className='home'>
+//             <TenantNavbar/>
+//         <h1>Tenant User Homepage</h1>
+//     </div>
+//   );
+// }
+
+class tenantHome extends Component { 
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/tenant_get_photo_notification")
+    .then(
+        res => {
+            console.log(res);
+
+        }
+    )
+  }
+
+  render() {
+    return (
+      <div className='home'>
+        <TenantNavbar/>
         <h1>Tenant User Homepage</h1>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default tenantHome;
