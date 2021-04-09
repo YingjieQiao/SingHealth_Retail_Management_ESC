91cc9605a37caee87347199812f4554df9df4bc3
24kmystique <56757916+24kmystique@users.noreply.github.com>
2021-04-09 13:35:27 +0800

    Remove username bypass

--- src/Components/auditChecklistNonFB.js
+++ src/Components/auditChecklistNonFB.js
@@ -31,16 +31,16 @@ class AuditChecklistNonFB extends Component {
 
     componentDidMount() {
         try {
-            // axios.get("http://localhost:5000/if_loggedin")
-            // .then(
-            //     res => {
-            //         console.log(res.data);
-            //         if(res.data.username==""){
-            //           alert("Please Log in!");
-            //           this.props.history.push('/');
-            //         }
-            //     }
-            // );
+            axios.get("http://localhost:5000/if_loggedin")
+            .then(
+                res => {
+                    console.log(res.data);
+                    if(res.data.username==""){
+                      alert("Please Log in!");
+                      this.props.history.push('/');
+                    }
+                }
+            );
             axios.get("http://localhost:5000/tenant_list_non_FB")
             .then(
                 res => {
