c8cdf8852edba9ff220759e57ff94e693641b9f8
Mihir Chhiber <mihir_chhiber@mymail.sutd.edu.sg>
2021-04-18 16:43:24 +0800

    covid checklist completd

--- backend/tests/test_covid_checklist.py
+++ backend/tests/test_covid_checklist.py
@@ -16,39 +16,38 @@ get db data:
 class TestAudit(TestBase):
 
     TEST_AUDIT_1 = {  #  Correct data with correct column names
-        "auditorName" : "Jerry",
-        "auditorDepartment" : "Risk",
-        "auditeeName" : "mihir_chhiber@mymail.sutd.edu.sg",
-        "workSafetyHealthScore" : 10,
-        "profStaffHydScore": 6,
-        "houseGeneralScore": 7
+        'auditorName' : "Jerry"
+        'auditorDepartment' : "Risk"
+        'auditeeName' : "mihir_chhiber@mymail.sutd.edu.sg"
+        'comment' : "The hygiene level needs attention otherwise suspension of lease of con..."
+        'checklist' : [1,0,-1,1,0,-1,1,0,-1,1,0,-1,1]
+
     }
 
     TEST_AUDIT_2 = {  # Missing data
-        "auditorName": "Jerry",
-        "auditorDepartment": "Risk",
-        "auditeeName": "mihir_chhiber@mymail.sutd.edu.sg",
-        "workSafetyHealthScore": 10,
-        "houseGeneralScore": 7
+        'auditorName' : "Jerry"
+        #department is missing
+        'auditeeName' : "mihir_chhiber@mymail.sutd.edu.sg"
+        'comment' : "The hygiene level needs attention otherwise suspension of lease of con..."
+        'checklist' : [1,0,-1,1,0,-1,1,0,-1,1,0,-1,1]
     }
 
     TEST_AUDIT_3 = {  # Extra data
-        "auditorName": "Jerry",
-        "auditorDepartment": "Risk",
-        "auditeeName": "mihir_chhiber@mymail.sutd.edu.sg",
-        "workSafetyHealthScore": 10,
-        "profStaffHydScore": 6,
-        "houseGeneralScore": 7,
-        "test": 123
+        'auditorName' : "Jerry"
+        'auditorDepartment' : "Risk"
+        'auditeeName' : "mihir_chhiber@mymail.sutd.edu.sg"
+        'comment' : "The hygiene level needs attention otherwise suspension of lease of con..."
+        'checklist' : [1,0,-1,1,0,-1,1,0,-1,1,0,-1,1]
+        "extra": 123
     }
 
     TEST_AUDIT_4 = {  # Incorrect data type
-        "auditorName": "Jerry",
-        "auditorDepartment": "Risk",
-        "auditeeName": "mihir_chhiber@mymail.sutd.edu.sg",
-        "workSafetyHealthScore": 10,
-        "profStaffHydScore": 6,
-        "houseGeneralScore": "this is wrong datatype"
+        'timestamp' : "2021-03-31 02:54:48.316355"
+        'auditorName' : "Jerry"
+        'auditorDepartment' : "Risk"
+        'auditeeName' : "mihir_chhiber@mymail.sutd.edu.sg"
+        'comment' : 1234 # should be string
+        'checklist' : [1,0,-1,1,0,-1,1,0,-1,1,0,-1,1]
     }
 
     TEST_AUDIT_1_JSON = json.dumps(TEST_AUDIT_1)
@@ -58,19 +57,19 @@ class TestAudit(TestBase):
 
 
     def test_audit_submit_pass_1(self):
-        rv = self.client.post('/auditChecklist', data=self.TEST_AUDIT_1_JSON,
+        rv = self.client.post('/covidChecklist', data=self.TEST_AUDIT_1_JSON,
                               content_type='application/json')
         assert rv.status_code == 200
         assert rv.json['statusText'] == True
 
     def test_audit_submit_fail_1(self):
-        rv = self.client.post('/auditChecklist', data=self.TEST_AUDIT_2_JSON,
+        rv = self.client.post('/covidChecklist', data=self.TEST_AUDIT_2_JSON,
                               content_type='application/json')
         assert rv.status_code == 500
         assert rv.json['statusText'] == False
 
     def test_audit_submit_fail_2(self):
-        rv = self.client.post('/auditChecklist', data=self.TEST_AUDIT_3_JSON,
+        rv = self.client.post('/covidChecklist', data=self.TEST_AUDIT_3_JSON,
                               content_type='application/json')
         assert rv.status_code == 500
         assert rv.json['statusText'] == False
